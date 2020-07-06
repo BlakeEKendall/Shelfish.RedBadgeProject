f696cbafcf22d2ff8f0cd50cf9616c15a7a90559
Blake Kendall <blake.e.kendall@gmail.com>
2020-07-06 17:43:59 -0400

    Created blank controllers for Books, Audiobooks, Authors, Bookshelves, and Reviews. Also added blank folders to the Models layer where I can start adding models for each class to be used through the services and the controllers.

---
+++ Shelfish.WebMVC/Controllers/AudiobookController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shelfish.WebMVC.Controllers
+{
+    public class AudiobookController : Controller
+    {
+        // GET: Audiobook
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Shelfish.WebMVC/Controllers/AuthorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shelfish.WebMVC.Controllers
+{
+    public class AuthorController : Controller
+    {
+        // GET: Author
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
