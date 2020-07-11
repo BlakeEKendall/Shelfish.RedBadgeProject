da0c987ba8c3dff672f2159fa90215f146c83c60
blake.e.kendall@gmail.com <blake.e.kendall@gmail.com>
2020-07-11 15:16:48 -0400

    Completed the ReviewListItem model to begin creating controllers and views.

--- Shelfish.Models/ReviewModels/ReviewListItem.cs
+++ Shelfish.Models/ReviewModels/ReviewListItem.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Shelfish.Data;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,14 @@ namespace Shelfish.Models.ReviewModels
 {
     public class ReviewListItem
     {
+        [Display(Name ="Review ID")]
+        public int ReviewId { get; set; }
+        public string Title { get; set; }
+        
+        [Display(Name ="Book Reviewed")]
+        public string BookTitle { get; set; }
+
+        [Display(Name ="Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
     }
 }
