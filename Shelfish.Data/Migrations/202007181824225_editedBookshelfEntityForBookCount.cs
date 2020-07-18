8c29e2f42205afb191800b3a2aa112114be72069
blake.e.kendall@gmail.com <blake.e.kendall@gmail.com>
2020-07-18 14:25:26 -0400

    Added migration to account for change in Bookshelf entity to get the TotalBooks property by running BooksOnShelf.Count();

---
+++ Shelfish.Data/Migrations/202007181824225_editedBookshelfEntityForBookCount.cs
@@ -0,0 +1,18 @@
+﻿namespace Shelfish.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class editedBookshelfEntityForBookCount : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Bookshelf", "TotalBooks");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Bookshelf", "TotalBooks", c => c.Int(nullable: false));
+        }
+    }
+}
