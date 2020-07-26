cef7054d15316127b4154d8e6efc97c412a34d28
blake.e.kendall@gmail.com <blake.e.kendall@gmail.com>
2020-07-26 13:35:03 -0400

    Images added to contact page, some styling for hover and color added as well to Site.css. Added font-awesome to BundleConfig.cs to help with project loading.

--- Shelfish.WebMVC/App_Start/BundleConfig.cs
+++ Shelfish.WebMVC/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace Shelfish.WebMVC
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/font-awesome.min.css"));
         }
     }
 }
